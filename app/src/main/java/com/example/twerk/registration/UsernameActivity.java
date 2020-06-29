cc400a2950f43b1fc8e666937f931a8213d5053b
Aniket Gupta <42040595+Aniket10081998@users.noreply.github.com>
2020-06-29 19:35:45 +0530

    Authentication using Firebase

---
+++ app/src/main/java/com/example/twerk/registration/UsernameActivity.java
@@ -0,0 +1,16 @@
+package com.example.twerk.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.twerk.R;
+
+public class UsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_username);
+    }
+}
